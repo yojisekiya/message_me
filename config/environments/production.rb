4043c0f39f868a548c3802a758e212f2dddb9704
Yoji Sekiya <yojisekiya@gmail.com>
2022-09-17 13:52:45 +0900

    change heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = ['https://boiling-chamber-77271.herokuapp.com/', 'https://boiling-chamber-77271.herokuapp.com/']
+  config.action_cable.allowed_request_origins = ['https://powerful-dusk-38667.herokuapp.com/', 'https://powerful-dusk-38667.herokuapp.com/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
